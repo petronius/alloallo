2522bc59ff69ce835db6b26abe81c9bae4fb4a8b
Kamil Kujawinski <kamil@kujawinski.net>
2015-06-20 15:15:56 +0200

    proper host

--- alloallo/alloallo/settings/production.py
+++ alloallo/alloallo/settings/production.py
@@ -5,4 +5,4 @@ from __future__ import unicode_literals, absolute_import
 
 from .heroku import *
 
-ALLOWED_HOSTS = ['http://alloallo.herokuapp.com/']
+ALLOWED_HOSTS = ['http://allo-allo.herokuapp.com/']
