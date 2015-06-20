b4068452cbd5fb84c1a3f224e6a6207561dabfd0
Marcin Najtkowski <marcin.najtkowski@catonthekeys.pl>
2015-06-20 17:26:52 +0200

    [#12] added audio_description field to Profile model

--- alloallo/profiles/models.py
+++ alloallo/profiles/models.py
@@ -17,6 +17,7 @@ class BaseProfile(models.Model):
                                 blank=True)
     bio = models.CharField("Short Bio", max_length=200, blank=True, null=True)
     email_verified = models.BooleanField("Email verified", default=False)
+    audio_description = models.CharField("Audio description", null=True)
 
     class Meta:
         abstract = True
