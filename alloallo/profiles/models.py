d5a81c489500920f8263da13a5f2d48ead48cd12
Marcin Najtkowski <marcin.najtkowski@catonthekeys.pl>
2015-06-20 17:28:32 +0200

    [#12] Updated Profile model, added migration

--- alloallo/profiles/models.py
+++ alloallo/profiles/models.py
@@ -17,7 +17,9 @@ class BaseProfile(models.Model):
                                 blank=True)
     bio = models.CharField("Short Bio", max_length=200, blank=True, null=True)
     email_verified = models.BooleanField("Email verified", default=False)
-    audio_description = models.CharField("Audio description", null=True)
+    audio_description = models.CharField(
+        "Audio description", max_length=1024, null=True
+    )
 
     class Meta:
         abstract = True
