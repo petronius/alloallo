cec5ca3105cef2c017c0107681f55c33de44e8b2
Tomasz Wozniak <wozmin@gmail.com>
2015-06-20 19:17:29 +0200

    fixed login issue

--- alloallo/accounts/forms.py
+++ alloallo/accounts/forms.py
@@ -5,16 +5,34 @@ from authtools import forms as authtoolsforms
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit, HTML, Field
 
-from django.contrib.auth.forms import AuthenticationForm
 from django import forms
 from django.contrib.auth import forms as authforms
 from django.core.urlresolvers import reverse
+from django.contrib.auth import authenticate
+from django.utils.translation import ugettext_lazy as _
 
 
-class LoginForm(AuthenticationForm):
-    remember_me = forms.BooleanField(required=False, initial=False)
+class LoginForm(forms.Form):
+    """
+    Base class for authenticating users. Extend this to get a form that accepts
+    username/password logins.
+    """
+    number = forms.CharField(max_length=254)
+    password = forms.CharField(label=_("Password"), widget=forms.PasswordInput)
 
-    def __init__(self, *args, **kwargs):
+    error_messages = {
+        'invalid_login': _("Please enter a correct %(username)s and password. "
+                           "Note that both fields may be case-sensitive."),
+        'inactive': _("This account is inactive."),
+    }
+
+    def __init__(self, request=None, *args, **kwargs):
+        """
+        The 'request' parameter is set for custom auth use by subclasses.
+        The form data comes in via the standard 'data' kwarg.
+        """
+        self.request = request
+        self.user_cache = None
         super(LoginForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
 
@@ -26,7 +44,50 @@ class LoginForm(AuthenticationForm):
             Field('remember_me'),
             Submit('sign_in', 'Log in',
                    css_class="btn btn-lg btn-primary btn-block"),
+        )
+
+    def clean(self):
+        username = self.cleaned_data.get('number')
+        password = self.cleaned_data.get('password')
+
+        if username and password:
+            self.user_cache = authenticate(number=username,
+                                           password=password)
+            if self.user_cache is None:
+                raise forms.ValidationError(
+                    self.error_messages['invalid_login'],
+                    code='invalid_login',
+                    params={'username': self.username_field.verbose_name},
+                )
+            else:
+                self.confirm_login_allowed(self.user_cache)
+
+        return self.cleaned_data
+
+    def confirm_login_allowed(self, user):
+        """
+        Controls whether the given User may log in. This is a policy setting,
+        independent of end-user authentication. This default behavior is to
+        allow login by active users, and reject login by inactive users.
+
+        If the given user cannot log in, this method should raise a
+        ``forms.ValidationError``.
+
+        If the given user may log in, this method should return None.
+        """
+        if not user.is_active:
+            raise forms.ValidationError(
+                self.error_messages['inactive'],
+                code='inactive',
             )
+
+    def get_user_id(self):
+        if self.user_cache:
+            return self.user_cache.id
+        return None
+
+    def get_user(self):
+        return self.user_cache
 
 
 class SignupForm(authtoolsforms.UserCreationForm):
