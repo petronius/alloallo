ed5da0c98b2fd872d1d27c67eae46016bb9139e8
Tomasz Wozniak <wozmin@gmail.com>
2015-06-21 01:03:50 +0200

    migration

---
+++ alloallo/accounts/migrations/0007_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accounts', '0006_user_friends'),
+        ('accounts', '0006_auto_20150620_1916'),
+    ]
+
+    operations = [
+    ]
